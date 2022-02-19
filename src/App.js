198a3686283811bece5cf3d8547434f00d86f511
ToniJukkola <n1juto00@students.oamk.fi>
2022-02-19 18:00:56 +0200

    Consoliin tulostuu tietoa.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,43 @@ import './App.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-const URL = 'https://movies-tvshows-data-imdb.p.rapidapi.com/';
-const API_KEY = '823622a3demsh181a48979d8f942p142ff5jsnf921c9c29740';
+const URL = 'http://www.omdbapi.com/';
+const API_KEY = '81981094';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-
+  const [items, setItems] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
   
   
   useEffect(() => {
-    
+    const parameter = '?t=tt';
+    const address = URL + parameter + "&apikey=" + API_KEY;
   
-  }, [])
+    axios.get(address)
+    .then((response) => {
+      ///console.log(response.data);
+      setItems(response.data.plot);
+      console.log(response.data);
+    }).catch(error => {
+      alert(error);
+    });
+
+  }, []);
     
   
 
   return (
  <div>
    <h3>Random movies</h3>
+   <div>
+       {items?.map(item =>(
+         <div key={item.title} onClick={e => setSelectedItem(item)}> 
+           <h3>{items.plot}</h3>
+           <p>{items.year}</p>
+         </div>
+     ))}
+     
+   </div>
  </div>
   );
 }

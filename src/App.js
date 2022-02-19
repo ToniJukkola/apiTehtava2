1e39ce7cb6adcabd18433286a2826100f77daf52
ToniJukkola <n1juto00@students.oamk.fi>
2022-02-19 23:03:23 +0200

    Api-kutsu toimii, muutama komponentti lisätty.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,68 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
+import Movies from './components/MovieList';
+import MovieListHeader from './components/MovieListHeader';
 
 const URL = 'http://www.omdbapi.com/';
 const API_KEY = '81981094';
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [selectedItem, setSelectedItem] = useState(null);
-  
-  
-  useEffect(() => {
-    const parameter = '?t=tt';
-    const address = URL + parameter + "&apikey=" + API_KEY;
-  
-    axios.get(address)
-    .then((response) => {
-      ///console.log(response.data);
-      setItems(response.data.plot);
-      console.log(response.data);
-    }).catch(error => {
-      alert(error);
-    });
+ // const [error, setError] = useState(null);
+  //const [isLoaded, setIsLoaded] = useState(false);
+  //const [items, setItems] = useState([]);
+ // const [selectedItem, setSelectedItem] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
+  const getMovieRequest = async () => {
+    const kutsu = URL + '?s=avengers' + '&apikey=' + API_KEY;
+
+    const response = await fetch(kutsu);
+    const responseJson = await response.json();
+
+    console.log(responseJson);
+    setMovies(responseJson.Search);
+  };
+
+  useEffect(()=>{
+    getMovieRequest();
   }, []);
-    
   
-
   return (
- <div>
-   <h3>Random movies</h3>
-   <div>
-       {items?.map(item =>(
-         <div key={item.title} onClick={e => setSelectedItem(item)}> 
-           <h3>{items.plot}</h3>
-           <p>{items.year}</p>
-         </div>
-     ))}
-     
-   </div>
- </div>
+    <div className='container-fluid movie-app'>
+      <div className='row'>
+        <MovieListHeader header='Search movies' />
+      </div>
+      <div className='row'>
+      <Movies movies={movies} />
+      </div>
+    </div>
   );
 }
+ 
+
+ /// if (error) {
+  ///  return <p>{error.message}</p>;
+  //}
+ /// else if (!isLoaded) {
+    ///return <p>Loading...</p>;
+ // }
+ /// else {
+   /// return (
+   /// <div>
+      ///  <h3>Random movies</h3>
+         /// {items?.map(item =>(
+            ///<div key={item.title} onClick={e => setSelectedItem(item)}> 
+             /// <h3>{items.plot}</h3>
+             /// <p>{items.year}</p>
+             /// <p>items</p>
+           /// </div>
+          ///))}
+     /// </div>
+    //  );
+  //  } 
+  
 
 export default App;
